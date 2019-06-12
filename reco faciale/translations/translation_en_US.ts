d510f7f1fd74c5e3091b1693dcb8310835d43cf6
jland972 <jeremy.cledelin@isen.yncrea.fr>
2019-06-12 11:10:00 +0200

    Add files via upload

---
+++ reco faciale/translations/translation_en_US.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Salut bro</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Salut bro</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>yo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">yo</translation>
+        </message>
+    </context>
+</TS>
